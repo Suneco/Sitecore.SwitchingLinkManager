b0bab3f1ff943ec42a162f98939475e8897b2658
Jeroen Speldekamp <Jspeldekamp@suneco.nl>
2016-12-23 16:38:03 +0100

    Init commit Example1 website

---
+++ Examples/Suneco.SwitchingLinkProvider.Example1/Src/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Suneco.SwitchingLinkProvider.Example1
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    /// <summary>
+    /// Contains routing tables settings 
+    /// </summary>
+    /// <seealso cref="System.Web.HttpApplication" />
+    public class MvcApplication : HttpApplication
+    {
+        /// <summary>
+        /// Applications the start.
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
