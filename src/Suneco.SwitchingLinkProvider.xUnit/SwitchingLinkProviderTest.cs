3fb3c126d64e51c7c36885787ac92b7a8d569790
Jeroen Speldekamp <Jspeldekamp@suneco.nl>
2016-12-23 10:51:01 +0100

    Adding Xunit test project to the solution

---
+++ src/Suneco.SwitchingLinkProvider.xUnit/SwitchingLinkProviderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Suneco.SwitchingLinkProvider.xUnit
+{
+    
+    public class SwitchingLinkProviderTest
+    {
+        [Fact]
+        public void Test001()
+        {
+            Assert.True(true,"It's working");
+        }
+    }
+}
