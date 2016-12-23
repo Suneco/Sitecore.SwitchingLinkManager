c6ba01d5aafb77c840f07d3f67980bdb1beb8aa7
Jeroen Speldekamp <Jspeldekamp@suneco.nl>
2016-12-23 11:17:48 +0100

    Rename project file

---
+++ src/Suneco.SwitchingLinkProvider.Test/SwitchingLinkProviderTests.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Suneco.SwitchingLinkProvider.Test
+{
+    using Xunit;
+
+    /// <summary>
+    /// Test class for testing the SwitchingLinkProvider class
+    /// </summary>
+    public class SwitchingLinkProviderTests
+    {
+        /// <summary>
+        /// Test001s this instance.
+        /// </summary>
+        [Fact]
+        public void Test001()
+        {
+            Assert.True(true,"It's working");
+        }
+    }
+}
